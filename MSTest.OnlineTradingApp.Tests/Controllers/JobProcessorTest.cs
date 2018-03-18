a3aa1af0c16ed2584350fd40d7f8d344372564a5
agnihotriketan <Ketan$123>
2018-03-18 12:19:32 +0530

    done

---
+++ MSTest.OnlineTradingApp.Tests/Controllers/JobProcessorTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTest.OnlineTradingApp.Controllers;
+using MSTest.OnlineTradingApp.Contract;
+using MSTest.OnlineTrandingApp.JobSchedular;
+using MSTest.OnlineTradingApp.DAL;
+using MSTest.OnlineTradingApp.Services;
+
+namespace MSTest.OnlineTradingApp.Tests.Controllers
+{
+    [TestClass]
+    public class JobProcessorTest
+    { 
+        //private readonly JobProcessor _jobProcessor;
+
+        //private IGenericRepository<TradeTransaction> _genericTradeTransactionRepository;
+        //public JobProcessorTest()
+        //{
+        //    this._jobProcessor = new JobProcessor(new MSTestTradingDBEntities());
+        //    this._genericTradeTransactionRepository = new GenericRepository<TradeTransaction>(new MSTestTradingDBEntities());
+        //}
+        //[TestMethod]
+        //public void ExecuteJob_ValidInput_success()
+        //{
+        //    // Arrange
+        //    HomeController controller = new HomeController();
+
+        //    // Act
+        //    ViewResult result = controller.Index() as ViewResult;
+
+        //    // Assert
+        //    Assert.IsNotNull(result);
+        //}
+         
+    }
+}
